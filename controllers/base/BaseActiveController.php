5e221bdf885c80edbfdcd2505321b5ee207294d5
Henry Ohanga <ohanga.henry@gmail.com>
2018-03-06 19:48:26 +0300

    Adds API base controllers

---
+++ controllers/base/BaseActiveController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @copyright Copyright (c) 2018 Code Particles
+ */
+
+namespace app\controllers\base;
+
+use yii\rest\ActiveController;
+
+/**
+ * BaseActiveController is the base controller for active controller
+ */
+class BaseActiveController extends ActiveController
+{
+    
+}
---
+++ controllers/base/BaseRestController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright Copyright (c) 2018 Code Particles
+ */
+
+namespace app\controllers\base;
+
+use yii\rest\Controller;
+
+/**
+ * BaseRestController is a base controller class for rest controllers
+ * 
+ * @author Henry Ohanga <ohanga.henry@gmail.com>
+ * @since 1.0
+ */
+class BaseRestController extends Controller
+{
+    
+}
