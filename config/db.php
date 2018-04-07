380ab6448e08162aa7281ac2b6fecf81c8cc0a7c
Henry Ohanga <ohanga.henry@gmail.com>
2018-04-07 13:25:28 +0300

    Adds prod db settings

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'yii2',
-    'password' => '',
+    'dsn' => 'mysql:host=particles.co.ke;dbname=particle_tour_of_heroes_backend',
+    'username' => 'particle_hero',
+    'password' => 'Hero1244#!',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
